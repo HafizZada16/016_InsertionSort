cda532bcfbb822d75c8b0c31f9148c74b9f50d3f
HafizZada16 <hafizzada16@gmail.com>
2024-03-14 17:38:11 +0700

    Menambahkan input

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan  Elemen Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
 void InsertionSort() {
     int i, n, temp, j;
     int arr[9];
@@ -19,6 +40,8 @@ void InsertionSort() {
         arr[j + 1] = temp;
     }
 }
+
+
 
 int main()
 {
